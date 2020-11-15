84f4878fead1a51f7eecee9600ddcba2e3cc8f16
Adam Rosenberg <arosenberg@mastercontrol.com>
2020-11-15 16:32:59 -0700

    init back

---
+++ backend/slope-finder/src/main/java/com/slopefinder/slopefinder/SlopeFinderApplication.java
@@ -0,0 +1,13 @@
+package com.slopefinder.slopefinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlopeFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlopeFinderApplication.class, args);
+	}
+
+}
---
+++ backend/slope-finder/src/test/java/com/slopefinder/slopefinder/SlopeFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slopefinder.slopefinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlopeFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
